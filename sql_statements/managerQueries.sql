cfbb6ec66f1f4dd5709d1724e3adb29c51bfe0f8
abigailibarrola <aibarrol@calpoly.edu>
2019-06-12 04:36:31 -0700

    added queries for manager (likely to be wrong however)

--- sql_statements/managerQueries.sql
+++ sql_statements/managerQueries.sql
@@ -1,5 +1,13 @@
 -- Library Manager View Queries
 
 -- Find all books checked out in a specified year
+SELECT DISTINCT title FROM Books, CheckoutHistories
+WHERE YEAR(checkout_date) = input_year;
+
+SELECT DISTINCT book_id FROM CheckoutHistories
+WHERE YEAR(checkout_date) = input_year;
 
 -- For each book, listed # times checked out in ea month of a year
+SELECT book_id, COUNT(book_id) as c_amnt FROM CheckoutHistories
+WHERE YEAR(checkout_date) = input_year
+GROUP BY MONTH(checkout_date);
