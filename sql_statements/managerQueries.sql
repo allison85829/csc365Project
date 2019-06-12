32392635a3468911a0d80fd4cdfc0a77b03a2b72
aquelle-cp <aquelle@calpoly.edu>
2019-06-12 16:33:53 -0700

    actually finished the library manager queries

--- sql_statements/managerQueries.sql
+++ sql_statements/managerQueries.sql
@@ -14,7 +14,7 @@ WHERE YEAR(checkout_date) = 2019
 GROUP BY MONTH(checkout_date);
 
 -- Usage history for the year by book with months for columns
-SELECT title,
+(SELECT title,
     SUM(CASE WHEN MONTH(checkout_date) = 1  THEN 1 ELSE 0 END) AS 'Jan',
     SUM(CASE WHEN MONTH(checkout_date) = 2 THEN 1 ELSE 0 END) AS 'Feb',
     SUM(CASE WHEN MONTH(checkout_date) = 3 THEN 1 ELSE 0 END) AS 'Mar',
@@ -27,11 +27,30 @@ SELECT title,
     SUM(CASE WHEN MONTH(checkout_date) = 10 THEN 1 ELSE 0 END) AS 'Oct',
     SUM(CASE WHEN MONTH(checkout_date) = 11 THEN 1 ELSE 0 END) AS 'Nov',
     SUM(CASE WHEN MONTH(checkout_date) = 12 THEN 1 ELSE 0 END) AS 'Dec',
-    COUNT(*) AS Total
+    COUNT(*) AS 'Year'
 FROM CheckoutHistories
 JOIN Books ON CheckoutHistories.book_id = Books.book_id
 WHERE YEAR(checkout_date) = 2019
-GROUP BY CheckoutHistories.book_id;
+GROUP BY CheckoutHistories.book_id)
+UNION
+SELECT '' AS 'title', '' AS 'Jan', '' AS 'Feb', '' AS 'Mar', '' AS 'Apr', '' AS 'May', '' AS 'Jun', '' AS 'Jul', '' AS 'Aug', '' AS 'Sep', '' AS 'Oct', '' AS 'Nov', '' AS 'Dec', '' AS 'Year'
+UNION
+(SELECT 'Totals' AS title,
+    SUM(CASE WHEN MONTH(checkout_date) = 1  THEN 1 ELSE 0 END) AS 'Jan',
+    SUM(CASE WHEN MONTH(checkout_date) = 2 THEN 1 ELSE 0 END) AS 'Feb',
+    SUM(CASE WHEN MONTH(checkout_date) = 3 THEN 1 ELSE 0 END) AS 'Mar',
+    SUM(CASE WHEN MONTH(checkout_date) = 4 THEN 1 ELSE 0 END) AS 'Apr',
+    SUM(CASE WHEN MONTH(checkout_date) = 5 THEN 1 ELSE 0 END) AS 'May',
+    SUM(CASE WHEN MONTH(checkout_date) = 6 THEN 1 ELSE 0 END) AS 'Jun',
+    SUM(CASE WHEN MONTH(checkout_date) = 7 THEN 1 ELSE 0 END) AS 'Jul',
+    SUM(CASE WHEN MONTH(checkout_date) = 8 THEN 1 ELSE 0 END) AS 'Aug',
+    SUM(CASE WHEN MONTH(checkout_date) = 9 THEN 1 ELSE 0 END) AS 'Sep',
+    SUM(CASE WHEN MONTH(checkout_date) = 10 THEN 1 ELSE 0 END) AS 'Oct',
+    SUM(CASE WHEN MONTH(checkout_date) = 11 THEN 1 ELSE 0 END) AS 'Nov',
+    SUM(CASE WHEN MONTH(checkout_date) = 12 THEN 1 ELSE 0 END) AS 'Dec',
+    COUNT(*) AS 'Year'
+FROM CheckoutHistories
+WHERE YEAR(checkout_date) = 2019);
 
 -- Get list of all currently checked out books (both do the same thing)
 SELECT title FROM Books WHERE availability = 0;
